e360321bf94263f98f0bfcfbf8ec3960f5f922f6
Oeng zhileang <122541947+oengzhileang@users.noreply.github.com>
2024-01-10 18:35:04 +0700

    Add files via upload

---
+++ src/ShortDataCoverter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ShortDataCoverter {
+     public static void main(String[] args) {
+        try (
+            Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Enter a short date (YYYY-MM-DD): ");
+                String shortDate = scanner.nextLine();
+    
+                int year = Integer.parseInt(shortDate.substring(0, 4));
+                int month = Integer.parseInt(shortDate.substring(5, 7));
+                int day = Integer.parseInt(shortDate.substring(8));
+    
+                String monthName = getMonthName(month);
+                System.out.println( "Full Date : " + monthName + " " + day + ", " + year);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+    }
+    private static String getMonthName(int month) {
+        String[] monthNames = {
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"
+        };
+
+        if (month >= 1 && month <= 12) {
+            return monthNames[month - 1];
+        } else {
+            return "Invalid Month";
+        }
+    }
+}
+
